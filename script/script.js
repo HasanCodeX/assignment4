48f0c89867822ab750cb87a672f0f27b976c23d5
Mehedi Hasan <2025mehedihasan@gmail.com>
2024-03-18 02:33:27 +0600

    banner figma

--- script/script.js
+++
@@ -1,5 +0,0 @@
- // JavaScript to handle button click
- document.getElementById("bannerButton").addEventListener("click", function() {
-    // You can add your logic here, for example, redirecting to another page:
-    window.location.href = "your_page_url.html";
-});
