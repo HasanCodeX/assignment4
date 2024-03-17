0a9b11db96228bb9e4b2c44feee3935d6816bd9b
Mehedi Hasan <2025mehedihasan@gmail.com>
2024-03-18 01:25:58 +0600

    banner start

---
+++ script/script.js
@@ -0,0 +1,5 @@
+ // JavaScript to handle button click
+ document.getElementById("bannerButton").addEventListener("click", function() {
+    // You can add your logic here, for example, redirecting to another page:
+    window.location.href = "your_page_url.html";
+});
