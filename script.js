5e14da441bfa4817509713ffe3771109847f2bf0
Mehedi Hasan <2025mehedihasan@gmail.com>
2024-03-26 10:26:23 +0600

    xyz

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 function showDetails(coupon) {
     var details = document.getElementById(coupon + "Details");
-    if (details.style.display === "none") {
-      details.style.display = "block";
+    var computedStyle = window.getComputedStyle(details);
+    if (computedStyle.display === "none") {
+        details.style.display = "block";
     } else {
-      details.style.display = "none";
+        details.style.display = "none";
     }
-  }
+}
 
-  function showAllOffers() {
+function showAllOffers() {
     var allOffers = document.getElementById("allOffers");
-    if (allOffers.style.display === "none") {
-      allOffers.style.display = "block";
+    var computedStyle = window.getComputedStyle(allOffers);
+    if (computedStyle.display === "none") {
+        allOffers.style.display = "block";
     } else {
-      allOffers.style.display = "none";
+        allOffers.style.display = "none";
     }
-  }
+}
+function showDetails(coupon) {
+    var details = document.getElementById(coupon + "Details");
+    var computedStyle = window.getComputedStyle(details);
+    if (computedStyle.getPropertyValue('display') === 'none') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+}
+
+function showAllOffers() {
+    var allOffers = document.getElementById("allOffers");
+    var computedStyle = window.getComputedStyle(allOffers);
+    if (computedStyle.getPropertyValue('display') === 'none') {
+        allOffers.style.display = 'block';
+    } else {
+        allOffers.style.display = 'none';
+    }
+}
